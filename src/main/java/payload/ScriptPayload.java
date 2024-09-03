5bcd44e784b4d9ac23dfea282440dd0ec3b02bbf
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-03 21:36:43 +0200

    Create mutation request for GraphQL, show new character id

--- src/main/java/payload/ScriptPayload.java
+++ src/main/java/payload/ScriptPayload.java
@@ -1,7 +1,11 @@
 package payload;
 
 public class ScriptPayload {
-    public static String bodyPayload() {
+    public static String bodyQueryPayload() {
         return "{\"query\":\"query($characterId : Int!, $episodeId: Int!) {\\n      \\n      character(characterId: $characterId) {\\n        name\\n        gender\\n        status\\n        id\\n      }\\n      \\n      location(locationId: $characterId) {\\n        name\\n        dimension\\n      }\\n      \\n      episode(episodeId: $episodeId) {\\n        name\\n        air_date\\n        episode\\n      }\\n      \\n      characters(filters: {name : \\\"Test\\\"}) {\\n        info {\\n          count\\n        }\\n        result {\\n          name\\n          species\\n          type\\n        }\\n      }\\n      episodes(filters: {episode: \\\"hulu\\\"}) {\\n        result {\\n          id\\n          name\\n          air_date\\n          episode\\n        }\\n      }\\n      \\n    }\",\"variables\":{\"characterId\":3366,\"episodeId\":3366}}";
+    }
+
+    public static String bodyMutationPayload() {
+        return "{\"query\":\"mutation {\\n  \\n\\tcreateLocation(location: {\\n    name: \\\"Bulgaria\\\",\\n    type: \\\"Balkans\\\",\\n    dimension: \\\"4D\\\"\\n  }) {\\n    id\\n  }\\n  \\n\\tcreateCharacter(character: {\\n    name: \\\"Omni-Man\\\",\\n    type: \\\"Evil\\\",\\n    status: \\\"Alive\\\",\\n    species: \\\"Viltrumite\\\",\\n    gender: \\\"Male\\\",\\n    image: \\\"https://static.wikia.nocookie.net/amazon-invincible/images/6/63/Omni-ManProfile.png\\\",\\n    originId: 13273\\n    locationId: 13273\\n  }) {\\n    id\\n  }\\n  \\n  \\n\\tcreateEpisode(episode: {\\n    name: \\\"It's About Time\\\",\\n    air_date: \\\"March 26, 2021\\\",\\n    episode: \\\"Pilot\\\"\\n  }) {\\n    id\\n  }\\n}\",\"variables\":null}";
     }
 }
--- src/test/java/graphql/GraphQLScript.java
+++ src/test/java/graphql/GraphQLScript.java
@@ -3,16 +3,20 @@ package graphql;
 import io.restassured.path.json.JsonPath;
 import org.testng.Assert;
 
+import java.util.Arrays;
+
 import static io.restassured.RestAssured.*;
-import static payload.ScriptPayload.bodyPayload;
+import static payload.ScriptPayload.bodyMutationPayload;
+import static payload.ScriptPayload.bodyQueryPayload;
 
 public class GraphQLScript {
     public static void main(String[] args) {
-        String response =
+        // Query
+        /*String response =
         given()
             .log().all()
             .header("Content-Type", "application/json")
-            .body(bodyPayload())
+            .body(bodyQueryPayload())
         .when()
             .post("https://rahulshettyacademy.com/gq/graphql")
         .then()
@@ -23,6 +27,23 @@ public class GraphQLScript {
         JsonPath js = new JsonPath(response);
         String characterName = js.getString("data.character.name");
         System.out.println(characterName);
-        Assert.assertEquals(characterName, "Test");
+        Assert.assertEquals(characterName, "Test");*/
+
+        // Mutation
+        String mutationResponse =
+        given()
+             .log().all()
+             .header("Content-Type", "application/json")
+             .body(bodyMutationPayload())
+        .when()
+             .post("https://rahulshettyacademy.com/gq/graphql")
+        .then()
+              .extract().response().asString()
+        ;
+        System.out.println(mutationResponse);
+
+        JsonPath jsm = new JsonPath(mutationResponse);
+        String newCharacterId = jsm.getString("data.createCharacter.id");
+        System.out.println("Character id: " + newCharacterId);
     }
 }
